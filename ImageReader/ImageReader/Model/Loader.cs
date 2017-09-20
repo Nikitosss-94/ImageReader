90a6206b29c4bf66c1838cd77cedf9357479edd6
Nikit <Nikitosss-94@mail.ru>
2017-09-20 14:00:43 +0500

    2

---
+++ ImageReader/ImageReader/Model/Loader.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Windows.Media.Imaging;
+
+namespace ImageReader.Model
+{
+    /// <summary>
+    /// Звгрузчик изображения
+    /// </summary>
+    class Loader
+    {
+        private WebClient client { get; set; }
+
+        /// <summary>
+        /// номер загружаемого изображения
+        /// </summary>
+        private int _it;
+        public int It
+        {
+            get { return _it; }
+            set { _it = value; }
+        }
+
+        /// <summary>
+        /// размер загружаемого изображения
+        /// </summary>
+        private long _maxSize;
+        public long MaxSize
+        {
+            get { return _maxSize; }
+            set { _maxSize = value; }
+        }
+
+        /// <summary>
+        /// Текущий размер загружаемого изображения
+        /// </summary>
+        private double _curSize;
+        public double CurSize
+        {
+            get { return _curSize; }
+            set { _curSize = value; }
+        }
+        
+        public delegate void UpdateProgress();
+        event UpdateProgress Update;
+        public delegate void BtmImage(BitmapImage btm, int it);
+        event BtmImage Image;
+
+           /// <summary>
+           /// старт загрузки картинки
+           /// </summary>
+           /// <param name="path">URL</param>
+           /// <param name="update">процедура обратного вызова для обновления ProgressBar</param>
+           /// <param name="image">процедура обратного вызова для получения картинки</param>
+           /// <param name="i">номер загружаемой картинки</param>
+           /// <returns></returns>
+        public bool Start(string path, UpdateProgress update, BtmImage image, int i)
+        {
+            Update = update;
+            Image = image;
+            client = new WebClient();
+            It = i;
+            client.DownloadProgressChanged += Client_DownloadProgressChanged;
+            client.DownloadDataCompleted += Client_DownloadDataCompleted;
+            try
+            {
+                client.DownloadDataAsync(new Uri(path));
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Stop();
+                return false;
+            }
+        }
+
+
+        /// <summary>
+        /// загрузка завершена
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Client_DownloadDataCompleted(object sender, DownloadDataCompletedEventArgs e)
+        {
+            try
+            {
+                byte[] data = e.Result;
+                BitmapImage image = new BitmapImage();
+                using (var mem = new MemoryStream(data))
+                {
+                    mem.Position = 0;
+                    image.BeginInit();
+                    image.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
+                    image.CacheOption = BitmapCacheOption.OnLoad;
+                    image.UriSource = null;
+                    image.StreamSource = mem;
+                    try
+                    {
+                        image.EndInit();
+                    }
+                    catch (Exception ex) { Image(null, It); return; }
+                }
+                image.Freeze();
+                Image(image, It);
+            }
+            catch (Exception ex) { }
+            finally
+            {
+                Stop();
+            }
+        }
+
+        /// <summary>
+        /// процент загрузки изображения
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            MaxSize = e.TotalBytesToReceive;
+            CurSize = e.BytesReceived;
+            Update();
+        }
+
+        /// <summary>
+        /// Остановка загрузки картинки
+        /// </summary>
+        public void Stop()
+        {
+            client.CancelAsync();
+            client.Dispose();
+        }
+    }
+}
+
--- ImageReader/ImageReader/ViewModel/MainWindowViewModel.cs
+++ ImageReader/ImageReader/ViewModel/MainWindowViewModel.cs
@@ -1,19 +1,43 @@
 ﻿using ImageReader.Command;
+using ImageReader.Model;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
+using System.Windows.Media.Imaging;
 
 namespace ImageReader.ViewModel
 {
     class MainWindowViewModel : ViewModelBase
     {
+
+        /// <summary>
+        /// Загрузчик 1 изображения
+        /// </summary>
+        public Loader Loader1 { get; set; }
+
+        /// <summary>
+        /// Загрузчик 2 изображения
+        /// </summary>
+        public Loader Loader2 { get; set; }
+
+        /// <summary>
+        /// Загрузчик 3 изображения
+        /// </summary>
+        public Loader Loader3 { get; set; }
+
         #region Binding
 
-        private int _progressLoad;
-        public int ProgressLoad
+        /// <summary>
+        /// Положение BrogressBar
+        /// </summary>
+        private double _progressLoad;
+        public double ProgressLoad
         {
             get { return _progressLoad; }
             set
@@ -23,19 +47,11 @@ namespace ImageReader.ViewModel
             }
         }
 
-        private int _maxSize;
-        public int MaxSize
-        {
-            get { return _maxSize; }
-            set
-            {
-                _maxSize = value;
-                OnPropertyChanged("MaxSize");
-            }
-        }
-
-        private string _image1;
-        public string Image1
+        /// <summary>
+        /// 1 Изображение
+        /// </summary>
+        private object _image1;
+        public object Image1
         {
             get { return _image1; }
             set
@@ -45,8 +61,11 @@ namespace ImageReader.ViewModel
             }
         }
 
-        private string _image2;
-        public string Image2
+        /// <summary>
+        /// 2 Изображение
+        /// </summary>
+        private object _image2;
+        public object Image2
         {
             get { return _image2; }
             set
@@ -56,8 +75,11 @@ namespace ImageReader.ViewModel
             }
         }
 
-        private string _image3;
-        public string Image3
+        /// <summary>
+        /// 3 Изображение
+        /// </summary>
+        private object _image3;
+        public object Image3
         {
             get { return _image3; }
             set
@@ -67,6 +89,9 @@ namespace ImageReader.ViewModel
             }
         }
 
+        /// <summary>
+        /// URL 1 изображения
+        /// </summary>
         private string _url1;
         public string URL1
         {
@@ -78,6 +103,9 @@ namespace ImageReader.ViewModel
             }
         }
 
+        /// <summary>
+        /// URL 2 изображения
+        /// </summary>
         private string _url2;
         public string URL2
         {
@@ -89,6 +117,9 @@ namespace ImageReader.ViewModel
             }
         }
 
+        /// <summary>
+        /// URL 3 изображения
+        /// </summary>
         private string _url3;
         public string URL3
         {
@@ -105,6 +136,9 @@ namespace ImageReader.ViewModel
 
         #region Command
 
+        /// <summary>
+        /// Загрузка 1 изображения
+        /// </summary>
         AppCommand _start1;
         public AppCommand StartLoadImage1
         {
@@ -113,11 +147,19 @@ namespace ImageReader.ViewModel
                 return _start1 ??
                     (_start1 = new AppCommand(() =>
                     {
-                        Image1 = URL1;
-                    }));
+                        if (!(Loader1 = new Loader()).Start(URL1, Progress, BtmImage, 1))
+                        {
+                            Loader1 = null;
+                            MessageBox.Show("Неверный URL");
+                        }
+                    }
+                    , () => (!string.IsNullOrWhiteSpace(URL1) & Loader1 == null)));
             }
         }
 
+        /// <summary>
+        /// Загрузка 2 изображения
+        /// </summary>
         AppCommand _start2;
         public AppCommand StartLoadImage2
         {
@@ -126,11 +168,19 @@ namespace ImageReader.ViewModel
                 return _start2 ??
                     (_start2 = new AppCommand(() =>
                     {
-                        Image2 = URL2;
-                    }));
+                        if (!(Loader2 = new Loader()).Start(URL2, Progress, BtmImage, 2))
+                        {
+                            Loader2 = null;
+                            MessageBox.Show("Неверный URL");
+                        }
+                    }
+                    , () => (!string.IsNullOrWhiteSpace(URL2) & Loader2 == null)));
             }
         }
 
+        /// <summary>
+        /// Загрузка 3 изображения
+        /// </summary>
         AppCommand _start3;
         public AppCommand StartLoadImage3
         {
@@ -139,11 +189,40 @@ namespace ImageReader.ViewModel
                 return _start3 ??
                     (_start3 = new AppCommand(() =>
                     {
-                        Image3 = URL3;
-                    }));
+                        if (!(Loader3 = new Loader()).Start(URL3, Progress, BtmImage, 3))
+                        {
+                            Loader3 = null;
+                            MessageBox.Show("Неверный URL");
+                        }
+                    }
+                    , () => (!string.IsNullOrWhiteSpace(URL3) & Loader3 == null)));
             }
         }
 
+        /// <summary>
+        /// Загрузка всех изображений
+        /// </summary>
+        AppCommand _startAll;
+        public AppCommand StartAll
+        {
+            get
+            {
+                return _startAll ??
+                    (_startAll = new AppCommand(() =>
+                    {
+                        if (!string.IsNullOrWhiteSpace(URL1) & Loader1 == null) (Loader1 = new Loader()).Start(URL1, Progress, BtmImage, 1);
+                        if (!string.IsNullOrWhiteSpace(URL2) & Loader2 == null) (Loader2 = new Loader()).Start(URL2, Progress, BtmImage, 2);
+                        if (!string.IsNullOrWhiteSpace(URL3) & Loader3 == null) (Loader3 = new Loader()).Start(URL3, Progress, BtmImage, 3);
+                    }
+                    , () => (!string.IsNullOrWhiteSpace(URL1) & Loader1 == null) | 
+                    (!string.IsNullOrWhiteSpace(URL2) & Loader2 == null) | 
+                    (!string.IsNullOrWhiteSpace(URL3) & Loader3 == null)));
+            }
+        }
+
+        /// <summary>
+        /// Остановка загрузки 1 изображения
+        /// </summary>
         AppCommand _stop1;
         public AppCommand StopLoadImage1
         {
@@ -152,10 +231,18 @@ namespace ImageReader.ViewModel
                 return _stop1 ??
                     (_stop1 = new AppCommand(() =>
                     {
-                    }));
+                        Loader1.Stop();
+                        Loader1 = null;
+                        Progress();
+                    },
+                    () => Loader1 != null
+                    ));
             }
         }
 
+        /// <summary>
+        /// Остановка загрузки 2 изображения
+        /// </summary>
         AppCommand _stop2;
         public AppCommand StopLoadImage2
         {
@@ -164,10 +251,18 @@ namespace ImageReader.ViewModel
                 return _stop2 ??
                     (_stop2 = new AppCommand(() =>
                     {
-                    }));
+                        Loader2.Stop();
+                        Loader2 = null;
+                        Progress();
+                    },
+                    () => Loader2 != null
+                    ));
             }
         }
 
+        /// <summary>
+        /// Остановка загрузки 3 изображения
+        /// </summary>
         AppCommand _stop3;
         public AppCommand StopLoadImage3
         {
@@ -176,11 +271,57 @@ namespace ImageReader.ViewModel
                 return _stop3 ??
                     (_stop3 = new AppCommand(() =>
                     {
-                    }));
+                        Loader3.Stop();
+                        Loader3 = null;
+                        Progress();
+                    },
+                    () => Loader3 != null
+                    ));
             }
         }
 
         #endregion
         
+        /// <summary>
+        /// Получение загруженного изображения
+        /// </summary>
+        /// <param name="image">Изображение</param>
+        /// <param name="i">Номер изображения</param>
+        private void BtmImage(BitmapImage image, int i)
+        {
+            switch (i)
+            {
+                case 1:
+                    Image1 = image;
+                    Loader1 = null;
+                    break;
+                case 2:
+                    Image2 = image;
+                    Loader2 = null;
+                    break;
+                case 3:
+                    Image3 = image;
+                    Loader3 = null;
+                    break;
+            }
+            if (image == null) MessageBox.Show("Указанный URL " + i + " не является картинкой");
+            Progress();
+        }
+
+        /// <summary>
+        /// Обновление ProgressBar
+        /// </summary>
+        private void Progress()
+        {
+            var m = 0;
+            double p = 0;
+            m += Loader1 != null ? (int)Loader1.MaxSize : 0;
+            m += Loader2 != null ? (int)Loader2.MaxSize : 0;
+            m += Loader3 != null ? (int)Loader3.MaxSize : 0;
+            p += Loader1 != null ? Loader1.CurSize : 0;
+            p += Loader2 != null ? Loader2.CurSize : 0;
+            p += Loader3 != null ? Loader3.CurSize : 0;
+            ProgressLoad = (p * 100 / m);       
+        }
     }
 }
